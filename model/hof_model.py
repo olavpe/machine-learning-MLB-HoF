535a60b43c84b11c5737be65f186a61d28d2a8ff
Olav Landmark Pedersen <olavpe@gmail.com>
2020-11-09 19:11:11 +0100

    hopefully functional network

--- model/hof_model.py
+++ model/hof_model.py
@@ -1,42 +1,106 @@
-#!/usr/bin/env conda run -n AIMethods python
 import pandas as pd
 import numpy as np
-import mnist
+# import mnist
+from sklearn.preprocessing import LabelEncoder
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import Model
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.utils import to_categorical
+from keras_pandas.Automater import Automater
+
+### Importing the data
+
+train_df = pd.read_csv('../data/train_data.csv')
+test_df = pd.read_csv('../data/test_data.csv')
+data_type_dict = {'numerical': [ 'G_all', 'finalGame', 'OPS', 'Years_Played',
+                                 'Most Valuable Player', 'AS_games', 'Gold Glove',
+                                 'Rookie of the Year', 'World Series MVP', 'Silver Slugger'],
+                  'categorical': ['HoF']}
+# train_X = np.empty([0])
+# train_y = np.empty([0])
+# for index, row in train_df.iterrows():
+#     # train_X = np.array([train_X, row])
+#     train_X = np.array([train_X, row])
 
 
-# The first time you run this might be a bit slow, since the
-# mnist package has to download and cache the data.
-train_images = mnist.train_images()
-train_labels = mnist.train_labels()
-test_images = mnist.test_images()
-test_labels = mnist.test_labels()
 
-print(train_images.shape) # (60000, 28, 28)
-tf.print("Train images: ", train_images) # (60000,)
-print(train_labels.shape) # (60000,)
-tf.print("Train labels: ", train_labels) # (60000,)
+# encoder = LabelEncoder()
+# encoder.fit(train_)
 
+### Selecting the data to be used
+auto = Automater(data_type_dict=data_type_dict, output_var='HoF')
+auto.fit(train_df)
+# train_X, train_y = auto.fit_transform(train_df)
+train_X, train_y = auto.transform(train_df)
+test_X, test_y = auto.transform(test_df, df_out=False)
+print('train_X: ', train_X)
+# print('train_X: ', train_X)
+# print('train_y: ', train_y.head())
+# print('train_y: ', train_y.shape)
+# print('test_X: ', test_X.shape)
+# print('test_y: ', test_y.shape)
+## Testing with Keras-pandas
+x = auto.input_nub # Starting model with input nub
+print('first input nub: ', x)
+# x = Dense(10, activation='relu', input_shape=(10,))(x)
+# x = Dense(32, activation='relu')(x)
+# x = auto.output_nub(x, activation='sigmoid')
+x = auto.output_nub(x)
+print('output nub: ', x)
 
-model = Sequential([
-    Dense(10, activation='relu', input_shape=(10,)),
-    Dense(32, activation='relu'),
-    Dense(1, activation='sigmoid'),
-])
-
+model = Model(inputs=auto.input_layers, outputs=x)
 model.compile(
     optimizer='adam',
-    loss='binary_crossentropy',
+    loss=auto.suggest_loss(),
+    # loss='binary_crossentropy',
     metrics=['accuracy']
 )
+# Training the model
+model.fit(train_X, train_y, epochs = 5, batch_size = 32, validation_split=.2)
+# Testing the model
+model.evaluate(test_X, test_y, verbose = 2)
+# Saving the model
+model.save_weights('model.h5')
 
-test_cat = to_categorical(test_labels)
-print("testing to_categorical", test_cat)
 
 
-# model.fit(
-#     train_data,
+
+
+# # # The first time you run this might be a bit slow, since the
+# # # mnist package has to download and cache the data.
+# # train_images = mnist.train_images()
+# # train_labels = mnist.train_labels()
+# # test_images = mnist.test_images()
+# # test_labels = mnist.test_labels()
+
+
+# # model = Sequential([
+# #     Dense(10, activation='relu', input_shape=(10,)),
+# #     Dense(32, activation='relu'),
+# #     Dense(1, activation='sigmoid'),
+# # ])
+
+# # model.compile(
+# #     optimizer='adam',
+# #     loss='binary_crossentropy',
+# #     metrics=['accuracy']
+# # )
+
+# print('train_X length: ', len(train_X))
+# print('train_X[0] length: ', len(train_X[0]))
+# # print('train_X[0][0] length: ', len(train_X[0][0]))
+# print('test_X length: ', len(test_X))
+# print('test_X[0] length: ', len(test_X[0]))
+# # print('test_X[0][0] length: ', len(test_X[0][0]))
+# # print(test_X.shape) # (60000, 28, 28)
+# # tf.print("Test X: ", test_X) # (60000,)
+# # print(train_y.shape) # (60000,)
+# # tf.print("Train y: ", train_y) # (60000,)
+# # test_cat = to_categorical(test_y)
+# # print("testing to_categorical", test_cat)
+
+
+# # model.fit(
+# #     train_data,
--- src/filter_data.py
+++ src/filter_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from numpy.random import RandomState
+from sklearn.preprocessing import StandardScaler
 
 ### ---------------- Importing the data ---------------- ###
 
@@ -16,19 +17,24 @@ finalGameYear = df['finalGame'].str.split(pat="-", expand=True)[0]
 df['finalGame'] = finalGameYear
 # Removing NaN fields from the dataset
 df = df[finalGameYear.notnull()]
-# print(df.head())
 # Adding back to dataset as ints
-df['finalGame'] = df['finalGame'].astype(int)
+df['finalGame'] = df['finalGame'].astype('float32')
 
 
-### Fixing awards parts
+### Fixing parts that cannot have NaN
 
+
+##Awards parts
 # Replacing NaN in each column with 0 and adding back as type
 award_names = ['Most Valuable Player', 'World Series MVP', 'AS_games', 'Gold Glove',
-               'Rookie of the Year', 'Silver Slugger']
-for award in award_names:
-    df[award] = df[award].fillna(0)
-    df[award] = df[award].astype(int)
+               'Rookie of the Year', 'Silver Slugger','G', 'AB', 'R', 'H', '2B', '3B',
+               'HR', 'RBI', 'SB', 'BB', 'SO', 'IBB', 'HBP', 'SH', 'SF']
+for col in award_names:
+    df[col] = df[col].fillna(0)
+    df[col] = df[col].astype('float32')
+
+# df['HoF'] = df['HoF'].fillna('N')
+# df['HoF'] = df['HoF'].astype(string)
 
 
 ### Creating 1B, SLG, OBS, OPS
@@ -38,6 +44,18 @@ df['SLG'] = (df['1B'] + 2*df['2B'] + 3*df['3B'] + 4*df['HR'])/df['AB']
 df['OBS'] = (df['H'] + df['BB'] + df['HBP'])/(df['AB'] + df['BB']+ df['HBP']+ df['SF'])
 df['OPS'] = df['SLG'] + df['OBS']
 
+new_stats = ['1B', 'SLG', 'OBS', 'OPS']
+for col in new_stats:
+    df[col] = df[col].fillna(0)
+    df[col] = df[col].astype('float32')
+
+stat_names = ['G', 'AB', 'R', 'H', '2B', '3B', 'HR', 'RBI', 'SB', 'BB', 'SO', 'IBB', 'HBP', 'SH', 'SF', '1B', 'OPS', 'SLG', 'OBS']
+
+for name in stat_names:
+    string = 'number of NaN values in '+ name
+    print(string, df[name].isna().sum())
+# print('number of NaN values in SLG: ', df['SLG'].isna().sum())
+# print('number of NaN values in OBS: ', df['OBS'].isna().sum())
 
 ### Removing players who have played less than 10 years
 
@@ -47,6 +65,10 @@ print("length after removing players less than 10 yrs", len(df['playerID']))
 
 print(df[['playerID','HoF']].head())
 
+# df.to_csv('../data/examine_data.csv')
+
+pre_std_df = df
+
 # ### Separating HoF with non-HoF players
 # reg_df = df[df['HoF'].isnull()]
 # print("Eligible players not in Hall of Fame", len(reg_df['playerID']))
@@ -55,14 +77,39 @@ print(df[['playerID','HoF']].head())
 # print("Players in the hall of fame", len(hof_df['playerID']))
 # print(hof_df[['playerID','HoF']].head())
 
+
+### Selecting variables to use in training
+
+# df = df[['playerID', 'HoF', 'G_all', 'finalGame', 'OPS',
+df = df[['G_all', 'finalGame', 'OPS',
+         'Years_Played', 'Most Valuable Player', 'AS_games',
+         'Gold Glove', 'Rookie of the Year', 'World Series MVP', 'Silver Slugger',]]
+
+print('df: ', df)
+print()
+### Standardizing the data
+
+df = pd.DataFrame(StandardScaler().fit_transform(df), columns=df.columns)
+print('df: ', df)
+df['HoF'] = pre_std_df['HoF']
+print('df: ', df)
+
+### Converting HoF back to strings
+df['HoF'] = df['HoF'].replace(np.nan, 'N', regex=True)
+df['HoF'] = df['HoF'].replace(1.0, 'Y', regex=True)
+df['HoF'] = df['HoF'].replace(0.0, 'N', regex=True)
+
 ### Separating into test and training data
 
-# Creating proper random seed number
 seeded_random = RandomState(1)
 train_frac = 0.8
 train_df = df.sample(frac=train_frac, random_state=seeded_random)
 test_df = df.drop(train_df.index)
 
+
+########## Save the different testing and validation data sets. !!!!!!!
+train_df.to_csv('../data/train_data.csv')
+test_df.to_csv('../data/test_data.csv')
 
 print("train_df : ", train_df)
 print("test_df : ", test_df)
