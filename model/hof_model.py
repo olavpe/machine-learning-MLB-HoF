e342f71c4fddd1a08ffbd5fa41a1f3ed28f683a6
Olav Landmark Pedersen <olavpe@gmail.com>
2020-11-11 12:50:57 +0100

    functional confusion matrix data and first ROC curve

--- model/hof_model.py
+++ model/hof_model.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, roc_curve
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import Model
@@ -8,6 +10,18 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.utils import to_categorical
 from keras_pandas.Automater import Automater
+
+### Helper functions
+def plot_roc_curve(fper, tper, name):
+    plt.plot(fper, tper, color='orange', label='ROC')
+    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend()
+    file_name = '../result/ROC_curve_' + name + '.png'
+    plt.savefig(file_name)
+    plt.show()
 
 ### Importing the data
 
@@ -28,38 +42,15 @@ test_y_raw = test_df['HoF']
 ### Converting pandas arrays to numpy arrays
 train_X = train_X_raw.to_numpy()
 test_X = test_X_raw.to_numpy()
-print("train_X: ", train_X)
-print("test_X: ", test_X)
+# print("train_X: ", train_X)
+# print("test_X: ", test_X)
 
 ### Creating the label data for the train and test sets
 encoder = LabelEncoder()
-encoder.fit(train_y_raw)
-train_y = encoder.transform(train_y_raw)
-print("train_y: ", train_y)
-encoder = LabelEncoder()
-encoder.fit(test_y_raw)
-test_y = encoder.transform(test_y_raw)
+train_y = encoder.fit_transform(train_y_raw)
+test_y = encoder.fit_transform(test_y_raw)
 
-# train_X = []
-# # train_y = []
-# for index, row in train_X_raw.iterrows():
-#     train_X.append(np.array([row]))
-# # print("train_X: ", train_X)
-
-# test_X = []
-# # test_y = []
-# for index, row in test_X_raw.iterrows():
-#     test_X.append(np.array([row]))
-# print("test_X: ", test_X)
-
-# encoder = LabelEncoder()
-# encoder.fit(train_)
-
-### Selecting the data to be used
-# train_X, train_y = auto.transform(train_df)
-# test_X, test_y = auto.transform(test_df, df_out=False)
-# print('train_X: ', train_X)
-
+### Creating and compiling the model
 model = Sequential([
     Dense(10, activation='relu', input_shape=(10,)),
     Dense(32, activation='relu'),
@@ -73,12 +64,27 @@ model.compile(
 )
 
 # Training the model
-model.fit(train_X, train_y, epochs = 5, batch_size=1, validation_split=.2)
+# model.fit(train_X, train_y, epochs = 5, batch_size=1, validation_split=.2)
+model.load_weights('model.h5')
 # Testing the model
 model.evaluate(test_X, test_y, verbose = 2)
-predictions = model.predict(test_X[94:104])
+full_predictions = model.predict_classes(test_X)
 
-print(np.argmax(predictions, axis=1)) # [7, 2, 1, 0, 4]
+### Examining metrics
+print('number of HoF players in data_set: ', test_y.sum())
+tn, fp, fn, tp = confusion_matrix(test_y, full_predictions).ravel()
+confusion_metrics = [tn, fp, fn, tp]
+confusion_label = ["tn", "fp", "fn", "tp"]
+for i in range(0,len(confusion_metrics)):
+    print(confusion_label[i], ': ', confusion_metrics[i])
+
+# full_predictions = full_predictions[:,1]
+fper, tper, thresholds = roc_curve(test_y, full_predictions)
+print("fper: ", fper)
+print("tper: ", tper)
+plot_roc_curve(fper, tper, "intial_results")
+# small_predictions = model.predict(test_X[94:104])
+# print(np.argmax(small_predictions, axis=1)) # [7, 2, 1, 0, 4]
 
 # Saving the model
 model.save_weights('model.h5')
