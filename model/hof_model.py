878b6daa7786df1404480f637d54847b49683330
Olav Landmark Pedersen <olavpe@gmail.com>
2020-11-12 15:40:01 +0100

    cleaned up some code

--- model/hof_model.py
+++ model/hof_model.py
@@ -28,16 +28,14 @@ HP = {
 
 ### Adding the information to the log file
 with open("../result/master_log.txt", "a") as file:
-    # log_header = print(HP)
-    # log_header = log_header + "\n"
     file.write("\n")
     file.write("\n")
     print(HP, file=file)
-    # file.write(log_header)
 
 
 ### Setting up the logger
 csv_logger = CSVLogger('../result/master_log.txt', append=True, separator=';')
+
 
 ### Helper functions
 def plot_roc_curve(fper, tper, name):
@@ -52,16 +50,14 @@ def plot_roc_curve(fper, tper, name):
     plt.show()
 
 
-### Importing the data
+### ---------------- Importing the data ---------------- ###
 
 train_df = pd.read_csv('../data/train_data_' + HP['NAME'] + '.csv', index_col=False)
 test_df = pd.read_csv('../data/test_data_' + HP['NAME'] + '.csv', index_col=False)
-# test_df = pd.read_csv('../data/test_data_60-40.csv', index_col=False)
 data_type_dict = {'numerical': [ 'G_all', 'finalGame', 'OPS', 'Years_Played',
                                  'Most Valuable Player', 'AS_games', 'Gold Glove',
                                  'Rookie of the Year', 'World Series MVP', 'Silver Slugger'],
                   'categorical': ['HoF']}
-
 
 
 ### Removing the answers for the input data
@@ -73,15 +69,15 @@ test_y_raw = test_df['HoF']
 ### Converting pandas arrays to numpy arrays
 train_X = train_X_raw.to_numpy()
 test_X = test_X_raw.to_numpy()
-# print("train_X: ", train_X)
-# print("test_X: ", test_X)
 
 ### Creating the label data for the train and test sets
 encoder = LabelEncoder()
 train_y = encoder.fit_transform(train_y_raw)
 test_y = encoder.fit_transform(test_y_raw)
 
-### Creating and compiling the model
+
+### ---------------- Creating and compiling the model ---------------- ###
+
 model = Sequential([
     Dense(10, activation='relu', input_shape=(10,)),
     Dense(32, activation='relu'),
@@ -95,25 +91,25 @@ model.compile(
 )
 
 # Training the model
-model.fit(train_X, train_y, epochs = HP['EPOCHS'], batch_size=HP['BATCH_SIZE'], validation_split=.2, callbacks=[csv_logger])
+model.fit(train_X, train_y, epochs = HP['EPOCHS'],
+          batch_size=HP['BATCH_SIZE'], validation_split=.2, callbacks=[csv_logger])
 # model.load_weights('model.h5')
+
 # Testing the model
 evaluation = model.evaluate(test_X, test_y, verbose = 2)
 full_predictions = model.predict_classes(test_X)
 
-### Examining metrics
-print('number of HoF players in data_set: ', test_y.sum())
+
+### ---------------- Examining metrics ---------------- ###
+
 tn, fp, fn, tp = confusion_matrix(test_y, full_predictions).ravel()
 confusion_metrics = [tn, fp, fn, tp]
 confusion_label = ["tn", "fp", "fn", "tp"]
 for i in range(0,len(confusion_metrics)):
     print(confusion_label[i], ': ', confusion_metrics[i])
 
-# full_predictions = full_predictions[:,1]
 fper, tper, thresholds = roc_curve(test_y, full_predictions)
 auroc = roc_auc_score(test_y, full_predictions)
-print("fper: ", fper)
-print("tper: ", tper)
 print("auroc: ", auroc)
 plot_roc_curve(fper, tper, HP['NAME'])
 # small_predictions = model.predict(test_X[94:104])
@@ -132,7 +128,7 @@ with open("../result/master_log.txt", "a") as file:
     print(metric_dict, file=file)
 
 
+### ---------------- Saving the model ---------------- ###
 
-# Saving the model
 model_weights_name = HP['NAME'] + '_model.h5'
 model.save_weights(model_weights_name)
--- src/filter_data.py
+++ src/filter_data.py
@@ -15,15 +15,13 @@ def num_sample(percentage, hof_df):
     hof_length = len(hof_df['G_all'])
     return int((percentage * hof_length)/(1 - percentage))
 
-### ---------------- Importing the data ---------------- ###
+
+### ---------------- Importing and cleaning the data ---------------- ###
 
 df = pd.read_csv('../data/final_data.csv')#,
                         # usecols=['playerID','nameFirst','nameLast','bats','throws','debut','finalGame'])
 
-print(df.columns)
-
 ### Fixing Last Game Played
-
 # Splits the final Game date and convert to string
 finalGameYear = df['finalGame'].str.split(pat="-", expand=True)[0]
 df['finalGame'] = finalGameYear
@@ -44,12 +42,10 @@ for col in award_names:
     df[col] = df[col].fillna(0)
     df[col] = df[col].astype('float32')
 
-# df['HoF'] = df['HoF'].fillna('N')
-# df['HoF'] = df['HoF'].astype(string)
 
+### ---------------- Creating additional statistics ---------------- ###
 
 ### Creating 1B, SLG, OBS, OPS
-
 df['1B'] = df['H'] - df['2B'] - df['3B'] - df['HR']
 df['SLG'] = (df['1B'] + 2*df['2B'] + 3*df['3B'] + 4*df['HR'])/df['AB']
 df['OBS'] = (df['H'] + df['BB'] + df['HBP'])/(df['AB'] + df['BB']+ df['HBP']+ df['SF'])
@@ -60,81 +56,46 @@ for col in new_stats:
     df[col] = df[col].fillna(0)
     df[col] = df[col].astype('float32')
 
-stat_names = ['G', 'AB', 'R', 'H', '2B', '3B', 'HR', 'RBI', 'SB', 'BB', 'SO', 'IBB', 'HBP', 'SH', 'SF', '1B', 'OPS', 'SLG', 'OBS']
-
+stat_names = ['G', 'AB', 'R', 'H', '2B', '3B', 'HR', 'RBI', 'SB',
+              'BB', 'SO', 'IBB', 'HBP', 'SH', 'SF', '1B', 'OPS', 'SLG', 'OBS']
 for name in stat_names:
     string = 'number of NaN values in '+ name
     print(string, df[name].isna().sum())
-# print('number of NaN values in SLG: ', df['SLG'].isna().sum())
-# print('number of NaN values in OBS: ', df['OBS'].isna().sum())
+
+
+### ---------------- Fulfilling criteria of HoF eligibility ---------------- ###
 
 ### Removing players who have played less than 10 years
-
 df = df[df['Years_Played'] >= 10] # 10 years
-print("length after removing players less than 10 yrs", len(df['playerID']))
-print("number of HoF before converting back to string", df['HoF'].sum())
-print('df: ', df)
-print('df[\'HoF\']: ', df['HoF'])
-# print("inspecting HoF", df['HoF'].describe())
-test_df = df[df["HoF"] == 1.0]
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print("length of test_df: ", len(test_df['HoF']))
-    # print("inspecting HoF", test_df['HoF'])
 
-print(df[['playerID','HoF']].head())
 
-# df.to_csv('../data/examine_data.csv')
-
-pre_std_df = df
-
+### ---------------- Preparation for input into model---------------- ###
 
 ### Selecting variables to use in training
+pre_std_df = df
 df = df[['G_all', 'finalGame', 'OPS',
          'Years_Played', 'Most Valuable Player', 'AS_games',
          'Gold Glove', 'Rookie of the Year', 'World Series MVP', 'Silver Slugger',]]
 
 ### Standardizing the data
 df = pd.DataFrame(StandardScaler().fit_transform(df), columns=df.columns)
-print("length of GHAHAHAHAdf: ", pre_std_df['HoF'].sum())
-
 ### Adding back the HoF data
 df.insert(df.shape[1], 'HoF', pre_std_df['HoF'].to_numpy())
-print('df: ', df)
-
-test_df = df[df["HoF"] == 1.0]
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print("MAYBEEEE???length of test_df: ", len(test_df['HoF']))
-    # print("inspecting HoF", test_df['HoF'])
-
 
 ### Converting HoF back to strings
 df['HoF'] = df['HoF'].replace(1.0, 'Y', regex=True)
-# print("inspecting HoF", df['HoF'].count())
 df['HoF'] = df['HoF'].replace(np.nan, 'N', regex=True)
 df['HoF'] = df['HoF'].replace(0.0, 'N', regex=True)
-
 
 ### Splitting dataset by HoF players
 reg_df = df[df['HoF'] == 'N']
 hof_df = df[df['HoF'] == 'Y']
 
-print('length of df: ', len(df))
-print('length of hof_df: ', len(hof_df))
-print('length of reg_df: ', len(reg_df))
-
 ### Under-sampling the non-HoF players
 reg_seeded_random = RandomState(1)
-
-
-# Trying a 50/50 split
-# sampled_reg_df = reg_df.sample(n = len(hof_df['G_all']), random_state=reg_seeded_random)
-# Trying a 60/40 split
 sampled_reg_df = reg_df.sample(n = num_sample(PERCENTAGE, hof_df), random_state=reg_seeded_random)
 
-print('length of sampled_reg_df: ', len(sampled_reg_df))
-# print('length of reg_df: ', len(reg_df))
-
-### Splitting reg and test into train and test
+### Splitting reg and HoF into train and test
 sep_seeded_random = RandomState(1)
 train_frac = 0.8
 train_reg_df = sampled_reg_df.sample(frac=train_frac, random_state=sep_seeded_random)
@@ -150,8 +111,6 @@ print('length of test_hof_df: ', len(test_hof_df))
 ### Merging the test and training datasets
 train_df = pd.concat([train_hof_df, train_reg_df])
 test_df = pd.concat([test_hof_df, test_reg_df])
-print('length of train_df: ', len(train_df))
-print('length of test_df: ', len(test_df))
 # Shuffling
 train_df = train_df.sample(frac = 1, random_state=sep_seeded_random)
 test_df = test_df.sample(frac = 1, random_state=sep_seeded_random)
@@ -162,13 +121,9 @@ test_df = test_df.sample(frac = 1, random_state=sep_seeded_random)
 # train_df = df.sample(frac=train_frac, random_state=sep_seeded_random)
 # test_df = df.drop(train_df.index)
 
+
+### ---------------- Saving data ---------------- ###
+
 ########## Save the different testing and validation data sets. !!!!!!!
-# train_df.to_csv('../data/train_data_60-40.csv', index=False)
-# test_df.to_csv('../data/test_data_60-40.csv', index=False)
 train_df.to_csv('../data/train_data_' + NAME + '.csv', index=False)
 test_df.to_csv('../data/test_data_' + NAME + '.csv', index=False)
-
-# print("train_df : ", train_df)
-# print("test_df : ", test_df)
-# print("train_df length: ", len(train_df))
-# print("test_df length: ", len(test_df))
